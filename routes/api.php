fb3ebee3ee0fc933d64df6b3af2fb0b25246cafb
uggy <91547970+ugnesipa@users.noreply.github.com>
2022-12-01 11:57:53 +0000

    updated model

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlantController;
 use App\Http\Controllers\ClimateController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MaintenanceRatingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,5 @@ Route::apiResource('/plants', PlantController::class);
 Route::resource('/categories', CategoryController::class)->only(['index', 'show']);
 //route to climates with only read functions (read all and read by id)
 Route::resource('/climates', ClimateController::class)->only(['index', 'show']);
+//route to maintenance ratings with only read functions (read all and read by id)
+Route::resource('/maintenance_ratings', MaintenanceRatingController::class)->only(['index', 'show']);
