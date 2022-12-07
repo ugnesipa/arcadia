34c2ce9e9d44d0debaecb30fd706d815e0d73a59
uggy <91547970+ugnesipa@users.noreply.github.com>
2022-12-07 14:18:56 +0000

    climates foreign key

--- app/Http/Controllers/MaintenanceRatingController.php
+++ app/Http/Controllers/MaintenanceRatingController.php
@@ -2,22 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Climate;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use App\Http\Resources\ClimateResource;
-use App\Http\Resources\ClimateCollection;
+use App\Models\MaintenanceRating;
+use App\Http\Resources\MaintenanceRatingResource;
+use App\Http\Resources\MaintenanceRatingCollection;
 
-class ClimateController extends Controller
-
-{ //code for swagger interpretation for displaying all climates
+class MaintenanceRatingController extends Controller
+{
     /**
      * Display a listing of the resource.
      *
      * @OA\Get(
-     *     path="/api/climates",
-     *     description="Displays all the climates",
-     *     tags={"Climates"},
+     *     path="/api/maintenance_ratings",
+     *     description="Displays all the Maintenace Rating",
+     *     tags={"MaintenanceRating"},
      *      @OA\Response(
      *          response=200,
      *          description="Successful operation, Returns a list of Climates in JSON format"
@@ -34,40 +32,34 @@ class ClimateController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-  //function to show all climates
+
+    //function to show all maintenance ratings
     public function index()
     {
-        return new ClimateCollection(Climate::all());
+        return new MaintenanceRatingCollection(MaintenanceRating::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-
-     *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\ClimateResource
+     * @return \Illuminate\Http\Response
      */
-     //function to store new climate if needed
     public function store(Request $request)
     {
-        // $climate = Climate::create($request->only([
-        //     'title', 'description'
-        // ]));
-
-        // return new ClimateResource($climate);
+        //
     }
-//code for swagger interpretation for displaying climates by id
+
     /**
      * Display the specified resource.
      *
      * @OA\Get(
-     *     path="/api/climates/{id}",
-     *     description="Gets a climate by ID",
-     *     tags={"Climates"},
+     *     path="/api/maintenance_ratings/{id}",
+     *     description="Gets a maintenance rating by ID",
+     *     tags={"MaintenanceRating"},
      *          @OA\Parameter(
      *          name="id",
-     *          description="Climate id",
+     *          description="Maintenance rating id",
      *          required=true,
      *          in="path",
      *          @OA\Schema(
@@ -87,45 +79,37 @@ class ClimateController extends Controller
      *      )
      * )
      *
-     * @param  \App\Models\Climate  $climate
-     * @return \Illuminate\Http\ClimateResource
+     * @param  \App\Models\MaintenanceRating  $maintenanceRating
+     * @return \Illuminate\Http\Response
      */
-       //function to show categories by id
-    public function show(Climate $climate)
+    //function to show maintenance ratings by id
+    public function show(MaintenanceRating $maintenanceRating)
     {
-        return new ClimateResource($climate);
+        return new MaintenanceRatingResource($maintenanceRating);
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Climate  $climate
+     * @param  \App\Models\MaintenanceRating  $maintenanceRating
      * @return \Illuminate\Http\Response
      */
-    //function to update climate if needed
-    public function update(Request $request, Climate $climate)
-    {
-        // $climate->update($request->only([
-        //     'title', 'description'
-        // ]));
 
-        // return new ClimateResource($climate);
+    //function to update maintenance rating if needed
+    public function update(Request $request, MaintenanceRating $maintenanceRating)
+    {
+        //
     }
 
     /**
      * Remove the specified resource from storage.
      *
-
-
-     *
-     * @param  \App\Models\Climate  $climate
+     * @param  \App\Models\MaintenanceRating  $maintenanceRating
      * @return \Illuminate\Http\Response
      */
-    //function to delete a climate by id if needed
-    public function destroy(Climate $climate)
+    public function destroy(MaintenanceRating $maintenanceRating)
     {
-        // $climate->delete();
-        // return response()->json(null, Response::HTTP_NO_CONTENT);
+        //
     }
 }
